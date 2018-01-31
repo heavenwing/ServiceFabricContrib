a853121e30756c28fe146fcec0b7b1fc596cb96a
朱永光 <heavenwing@msn.com>
2018-01-31 22:59:52 +0800

    update sfproj, improve ConfigurationProvider, change OwinCommunicationListener's target framework to net461

--- src/ConfigurationProvider/ServiceFabricConfigurationExtensions.cs
+++ src/ConfigurationProvider/ServiceFabricConfigurationExtensions.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Fabric;
+using System.Reflection;
 using System.Text;
 
 namespace ServiceFabricContrib
@@ -12,6 +14,37 @@ namespace ServiceFabricContrib
             ServiceContext serviceContext, string configPackageName = "Config")
         {
             builder.Add(new ServiceFabricConfigurationSource(serviceContext, configPackageName));
+            return builder;
+        }
+
+        /// <summary>
+        /// 添加JsonFile、SF、UserSecrets、EnvironmentVariables配置
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IWebHostBuilder UseCommonConfiguration(this IWebHostBuilder builder, ServiceContext serviceContext)
+        {
+            builder.ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+                config.AddServiceFabricConfiguration(serviceContext);
+
+                if (env.IsDevelopment())
+                {
+                    var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+                    if (appAssembly != null)
+                    {
+                        config.AddUserSecrets(appAssembly, optional: true);
+                    }
+                }
+
+                config.AddEnvironmentVariables();
+            });
+
             return builder;
         }
     }
