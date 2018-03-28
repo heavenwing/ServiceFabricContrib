daac3a4a30124de3f3f76a6d939418ce5f060c5d
Gavin Zhu <heavenwing@msn.com>
2018-03-28 10:52:27 +0800

    add mocks project

---
+++ src/Mocks/MockServiceProxy.cs
@@ -0,0 +1,46 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace ServiceFabricContrib
+{
+    using Microsoft.ServiceFabric.Services.Client;
+    using Microsoft.ServiceFabric.Services.Communication.Client;
+    using Microsoft.ServiceFabric.Services.Remoting;
+    using Microsoft.ServiceFabric.Services.Remoting.Client;
+    using Microsoft.ServiceFabric.Services.Remoting.V2.Client;
+    using System;
+    using System.Collections.Generic;
+
+    public class MockServiceProxy : IServiceProxy
+    {
+        private IDictionary<Type, Func<Uri, object>> createFunctions = new Dictionary<Type, Func<Uri, object>>();
+
+        public Type ServiceInterfaceType { get; private set; }
+
+        public IServiceRemotingPartitionClient ServicePartitionClient2 { get; private set; }
+
+
+        public TServiceInterface Create<TServiceInterface>(Uri serviceName) where TServiceInterface : IService
+        {
+            this.ServiceInterfaceType = typeof(TServiceInterface);
+            return (TServiceInterface)this.createFunctions[typeof(TServiceInterface)](serviceName);
+        }
+
+        //public TServiceInterface Create<TServiceInterface>(Uri serviceName, ServicePartitionKey key) where TServiceInterface : IService
+        //{
+        //    return (TServiceInterface)this.createFunctions[typeof(TServiceInterface)](serviceName);
+        //}
+
+        public TServiceInterface Create<TServiceInterface>(Uri serviceUri, ServicePartitionKey partitionKey = null, TargetReplicaSelector targetReplicaSelector = TargetReplicaSelector.Default, string listenerName = null) where TServiceInterface : IService
+        {
+            return (TServiceInterface)this.createFunctions[typeof(TServiceInterface)](serviceUri);
+        }
+
+        public void Supports<TServiceInterface>(Func<Uri, object> Create)
+        {
+            this.createFunctions[typeof(TServiceInterface)] = Create;
+        }
+    }
+}
---
+++ src/Mocks/MockTransaction.cs
@@ -0,0 +1,45 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace ServiceFabricContrib
+{
+    using Microsoft.ServiceFabric.Data;
+    using System;
+    using System.Threading.Tasks;
+
+    public class MockTransaction : ITransaction
+    {
+        public Task CommitAsync()
+        {
+            return Task.FromResult(true);
+        }
+
+        public void Abort()
+        {
+        }
+
+        public long TransactionId
+        {
+            get { return 0L; }
+        }
+
+        public long CommitSequenceNumber
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public Task<long> GetVisibilitySequenceNumberAsync()
+        {
+            return Task.FromResult(0L);
+        }
+    }
+}
