24b4df9a4c6d58d1b4a91560dc0bafb31596f4f1
Yongguang Zhu <yoz@microsoft.com>
2018-12-29 14:06:42 +0800

    add remotingservice class

---
+++ src/Common/StatefulRemotingService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ServiceFabric.Data;
+using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Text;
+
+namespace ServiceFabricContrib
+{
+    public abstract class StatefulRemotingService
+    {
+        public StatefulRemotingService(StatefulServiceContext serviceContext,
+          IReliableStateManager stateManager)
+        {
+            ServiceContext = serviceContext;
+            StateManager = stateManager;
+        }
+
+        protected StatefulServiceContext ServiceContext { get; }
+
+        protected IReliableStateManager StateManager { get; }
+    }
+}
---
+++ src/Common/StatelessRemotingService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Text;
+
+namespace ServiceFabricContrib
+{
+    public abstract class StatelessRemotingService
+    {
+        public StatelessRemotingService(StatelessServiceContext serviceContext)
+        {
+            ServiceContext = serviceContext;
+        }
+
+        protected StatelessServiceContext ServiceContext { get; }
+    }
+}
